1b20c741e93b82d74d41517d41dfb119e2f64b8b
jeff-powell-rakuten <jeff-powell-rakuten@users.noreply.github.com>
2015-05-27 12:15:23 +0100

    Update jcallmon-poller.php

--- jcallmon/jcallmon-poller.php
+++ jcallmon/jcallmon-poller.php
@@ -17,15 +17,15 @@
 
 $now = time();
 
-#$snmpreply=snmp2_walk("<<ENTER IP HERE>>", "<<ENTER SNMP COMMUNITY STRING HERE>>", "1.3.6.1.4.1.9.9.63.1.3.8.2");
+$snmpreply=snmp2_walk("<<ENTER IP HERE>>", "<<ENTER SNMP COMMUNITY STRING HERE>>", "1.3.6.1.4.1.9.9.63.1.3.8.2");
 
-#$dataline=explode( ":", $snmpreply[0]);
+$dataline=explode( ":", $snmpreply[0]);
 
-#$datavalue=$dataline[1];
+$datavalue=$dataline[1];
 
-#$callstot=ceil($datavalue/2);
+$callstot=ceil($datavalue/2);
 
-$callstot=rand(0,10);
+#$callstot=rand(0,10);
 
 $ret = rrd_update("/opt/jcallmon/data/calldb.rrd", "$now:$callstot");
 
